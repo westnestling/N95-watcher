49695bc2558f986adfbb905e617f15c97e0a303d
Daniel <424360793@qq.com>
2020-02-09 21:50:23 +0800

    [+]优化抢购功能 解决不能重定向等问题

--- tmall_order.py
+++ tmall_order.py
@@ -5,10 +5,16 @@ import os
 from selenium import webdriver
 import datetime
 import time
-
+from selenium.webdriver.chrome.options import Options
 
 # 创建浏览器对象
-driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__) + "/src", "chromedriver"))
+chrome_options = Options()
+# 关闭使用 ChromeDriver 打开浏览器时上部提示语 "Chrome正在受到自动软件的控制"
+chrome_options.add_argument("disable-infobars")
+# 允许浏览器重定向，Framebusting requires same-origin or a user gesture
+chrome_options.add_argument("disable-web-security")
+driver = webdriver.Chrome(os.path.join(os.path.dirname(__file__) + "/src", "chromedriver"),
+                          chrome_options=chrome_options)
 # 窗口最大化显示
 driver.maximize_window()
 
@@ -42,6 +48,7 @@ def buy(buy_time, mall):
     buy_time:购买时间
     mall:商城类别
     '''
+    print("开始购买")
     if mall == '1':
         # "立即购买"的css_selector
         btn_buy = '#J_juValid > div.tb-btn-buy > a'
@@ -55,17 +62,20 @@ def buy(buy_time, mall):
         # 现在时间大于预设时间则开售抢购
         if datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') > buy_time:
             try:
+                print("尝试下单")
                 # 找到“立即购买”，点击
                 if driver.find_element_by_css_selector(btn_buy):
                     driver.find_element_by_css_selector(btn_buy).click()
                     break
-                time.sleep(0.1)
+                # time.sleep(0.1)
             except:
-                time.sleep(0.3)
+                pass
 
+    print("下单成功")
     while True:
         try:
             # 找到“立即下单”，点击，
+            print("尝试提交订单")
             if driver.find_element_by_css_selector(btn_order):
                 driver.find_element_by_css_selector(btn_order).click()
                 # 下单成功，跳转至支付页面
@@ -75,14 +85,22 @@ def buy(buy_time, mall):
             time.sleep(0.5)
 
 
+# 使用方法
+# 1 设置url
+# 2 设置天猫还是淘宝
+# 3 设置开抢时间
+# 4 运行程序
+# 5 扫码登录
+# 6 选中要购买商品以及相应种类等（必须选中！！！）
+# 7 自动下单
 
 if __name__ == "__main__":
     # 输入要购买物品 url
-    url = "https://detail.tmall.com/item.htm?spm=a220m.1000858.1000725.6.5db97de7nIagyn&id=560599203455&skuId=3737496318597&areaId=320500&user_id=2261244963&cat_id=2&is_b=1&rn=45b3402ed4736ad0cd8928eafe32d986"
-    mall = input("请选择商城（淘宝 1  天猫 2  输入数字： ")
+    url = "https://detail.tmall.com/item.htm?id=559828657513"
+    # 请选择商城（淘宝 1  天猫 2  输入数字：
+    mall = '2'
     # 输入开售时间
-    bt = "2019-02-09 22:00:00"
+    bt = "2019-02-09 21:23:00"
     login(url, mall)
     buy(bt, mall)
     driver.quit()
-
