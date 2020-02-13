1bbc0b648e1989420aa8c27773a7aee327aac7b8
Daniel <424360793@qq.com>
2020-02-13 18:18:03 +0800

    [+]增加天猫超市购物车下单功能

--- tmall_order.py
+++ tmall_order.py
@@ -33,7 +33,7 @@ def login(url, mall):
     if mall == '1':
         # 找到并点击淘宝的登陆按钮
         driver.find_element_by_link_text("亲，请登录").click()
-    else:
+    elif mall == '2':
         # 找到并点击天猫的登陆按钮
         driver.find_element_by_link_text("请登录").click()
     print("请在30秒内完成登录")
@@ -54,9 +54,13 @@ def buy(buy_time, mall):
         btn_buy = '#J_juValid > div.tb-btn-buy > a'
         # "立即下单"的css_selector
         btn_order = '#submitOrder_1 > div.wrapper > a'
+    elif mall == '3':
+        btn_buy = '#J_Go'
+        btn_order = '#submitOrderPC_1 > div > a'
     else:
         btn_buy = '#J_LinkBuy'
         btn_order = '#submitOrderPC_1 > div > a'
+
     timeArray = time.strptime(buy_time, "%Y-%m-%d %H:%M:%S")
     # 转为时间戳
     timeStamp = int(time.mktime(timeArray))
@@ -65,29 +69,31 @@ def buy(buy_time, mall):
 
     while True:
         # 现在时间大于预设时间则开售抢购
-        if time.time() > timeStamp:
+        tmp_time = time.time()
+        if tmp_time >= timeStamp or (timeStamp - tmp_time) < 0.01:
             try:
-                print("尝试下单")
                 # 找到“立即购买”，点击
-                if driver.find_element_by_css_selector(btn_buy):
-                    driver.find_element_by_css_selector(btn_buy).click()
+                selector = driver.find_element_by_css_selector(btn_buy)
+                if selector:
+                    selector.click()
                     break
                 # time.sleep(0.1)
             except:
                 pass
-
-    print("下单成功")
     while True:
         try:
             # 找到“立即下单”，点击，
-            print("尝试提交订单")
-            if driver.find_element_by_css_selector(btn_order):
-                driver.find_element_by_css_selector(btn_order).click()
+            # print("尝试提交订单")
+            order_selector = driver.find_elements_by_css_selector(btn_order)
+            if order_selector:
+                order_selector[-1].click()
                 # 下单成功，跳转至支付页面
                 print("购买成功")
                 break
+            driver.refresh()
         except:
-            time.sleep(0.5)
+            driver.refresh()
+            time.sleep(0.01)
 
 
 # 使用方法
@@ -101,11 +107,12 @@ def buy(buy_time, mall):
 
 if __name__ == "__main__":
     # 输入要购买物品 url
-    url = "https://detail.tmall.com/item.htm?spm=a220m.1000858.1000725.1.449d5ee8L0oNmL&id=15839960304&areaId=320500&standard=1&user_id=101450072&cat_id=2&is_b=1&rn=baf2eb3ad6e052562ab3c4728367cfd3"
-    # 请选择商城（淘宝 1  天猫 2  输入数字：
-    mall = '2'
+    # url = "https://chaoshi.detail.tmall.com/item.htm?id=611911893371&sourceType=item&sourceType=item&price=126&suid=8d12be3f-0542-43be-a763-9822c43d5565&ut_sk=1.XbCIz/bFF4ADALGCBoZRXQj7_23181017_1581584224037.Copy.tm_detail&un=91e19e41e6228caa0a62cb2c046d31df&share_crt_v=1&spm=a2159r.13376460.0.0&sp_tk=77%20lNnA0YzFWM2FGWDfvv6U=&cpp=1&shareurl=true&short_name=h.VXpAOsV&sm=4a6d85&app=chrome"
+    url = "https://cart.tmall.com/cart.htm?from=bmini&tpId=725677994"
+    # 请选择商城（淘宝 1  天猫 2  3 天猫超市 输入数字：
+    mall = '3'
     # 输入开售时间
-    bt = "2020-02-13 11:23:59"
+    bt = "2020-02-13 00:00:00"
     login(url, mall)
     buy(bt, mall)
-    driver.quit()
+    # driver.quit()
